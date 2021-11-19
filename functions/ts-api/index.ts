7500ddacde03785fcedd0260ad491e39984dae86
Drew Kilb <drew.kilb@duke.edu>
2021-11-19 11:56:45 -0500

    feat: Connect to MySQL for a query

--- functions/ts-api/index.ts
+++ functions/ts-api/index.ts
@@ -1,10 +1,42 @@
-import { Handler } from "@netlify/functions";
+import {
+  HandlerContext,
+  HandlerEvent,
+  HandlerResponse,
+} from "@netlify/functions";
+import mysql from "mysql";
 
-const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ event: event }),
-  };
-};
+let pool = mysql.createPool(`${process.env.DATABASE_URL}?connectionLimit=5`);
+
+async function handler(
+  event: HandlerEvent,
+  context: HandlerContext
+): Promise<HandlerResponse> {
+  let pokemon = event.path.split("/")[-1];
+  if (!pokemon) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "Did not find Pokemon name to query." }),
+    };
+  }
+  return await new Promise((resolve, reject) => {
+    pool.query(
+      "select cast(id as char) as pokemonId, name, hp, legendary_or_mythical from pokemon where slug = ?",
+      [pokemon],
+      (error, results, fields) => {
+        if (error) {
+          resolve({
+            statusCode: 400,
+            body: JSON.stringify(error),
+          });
+        } else {
+          resolve({
+            statusCode: 200,
+            body: JSON.stringify(results),
+          });
+        }
+      }
+    );
+  });
+}
 
 export { handler };
